bd908112462a1ba5ec816e1fc9ada6ea5d8f5def
Tomoyuki Higuchi <h.tomoyuki@me.com>
2021-06-18 12:46:58 +0900

    add tests

--- tests/BoardTest.php
+++ tests/BoardTest.php
@@ -9,12 +9,29 @@ use PHPUnit\Framework\TestCase;
 class BoardTest extends TestCase
 {
 
+    private board $sut;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->sut = Board::make(3);
+    }
+
     /**
      * @test
      */
     public function ボードを生成できる() {
-        $board = Board::make(3);
+        $this->assertEquals("---\n---\n---\n", $this->sut->toString());
+    }
 
-        $this->assertEquals("---\n---\n---\n", $board->toString());
+    /**
+     * @test
+     */
+    public function 石を置ける() {
+        $position = new Position(1,1);
+        $this->sut->put($position, Stone::WHITE());
+
+        $this->assertEquals(Stone::WHITE(),$this->sut->get($position));
     }
 }
